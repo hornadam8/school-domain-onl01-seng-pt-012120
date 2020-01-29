10a8c8f5fe961daff8d50c5880e2c4869d79043e
Adam G Horn <hornadam8@gmail.com>
2020-01-29 16:49:03 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
-# code here!
+class School
+  
+  attr_reader :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(name,grade)
+    if @roster[grade] = []
+      @roster[grade] = [name]
+    else
+      @roster[grade] << name
+    end
+    @roster
+  end
+    
+  
+  
+end
